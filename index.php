2ea4a6fa9a5b5e63404de60979893855338071fa
L0uka <loukacadaulino@gmail.com>
2023-07-04 10:31:33 +0200

    add class BEM + h2

--- index.php
+++ index.php
@@ -1,30 +1,39 @@
 <?php
 
-require_once __DIR__.'/vendor/autoload.php';
-require_once __DIR__.'/Core/Helper.php';
-require_once __DIR__.'/Routes/Routes.php';
+// require_once __DIR__ . '/vendor/autoload.php';
+// require_once __DIR__ . '/Core/Helper.php';
+// require_once __DIR__ . '/Routes/Routes.php';
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>COGIP</title>
 </head>
+
 <body>
-    <nav>
-        <div class="nav">
-    <h1>COGIP</h1>
-    <a href="#">Home</a>
-    <a href="#">Invoices</a>
-    <a href="#">Compagnies</a>
-    <a href="#">Contacts</a>
+    <nav class="nav-bar">
+        <div class="nav-bar__link">
+            <h1>COGIP</h1>
+            <a href="#">Home</a>
+            <a href="#">Invoices</a>
+            <a href="#">Compagnies</a>
+            <a href="#">Contacts</a>
         </div>
-        <div class="sign_log">
-            <button type="submit">Sign Up</button>
-            <button type="submit">Login</button>
+        <div class="nav-bar__btn">
+            <button type="submit" class="nav-bar__btn--sign">Sign Up</button>
+            <button type="submit" class="nav-bar__btn--login">Login</button>
         </div>
     </nav>
+    <div class="">
+        <h2>MANAGE YOUR
+            CUSTOMERS AND
+            INVOICES EASLY</h2>
+    </div>
+
 </body>
+
 </html>
