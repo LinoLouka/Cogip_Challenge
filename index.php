1f0c713ac63ff0cda8eb816236e8d7e300d02bd4
ethanosaure <ethandiasmarques.pro@gmail.com>
2023-07-04 09:45:03 +0200

    adding nav bar

--- index.php
+++ index.php
@@ -4,3 +4,27 @@ require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/Core/Helper.php';
 require_once __DIR__.'/Routes/Routes.php';
 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COGIP</title>
+</head>
+<body>
+    <nav>
+        <div class="nav">
+    <h1>COGIP</h1>
+    <a href="#">Home</a>
+    <a href="#">Invoices</a>
+    <a href="#">Compagnies</a>
+    <a href="#">Contacts</a>
+        </div>
+        <div class="sign_log">
+            <button type="submit">Sign Up</button>
+            <button type="submit">Login</button>
+        </div>
+    </nav>
+</body>
+</html>
