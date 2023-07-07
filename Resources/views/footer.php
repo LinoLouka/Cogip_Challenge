692e161228ebe44527e7c2a6b93ca9505ba3d9e0
L0uka <loukacadaulino@gmail.com>
2023-07-07 14:32:18 +0200

    update links

--- Resources/views/footer.php
+++ Resources/views/footer.php
@@ -5,23 +5,29 @@
     </div>
     <div class="footer__infos">
         <div class="img_svg">
-        <img src="../../public/assets/img/maps.svg" alt="round_place"><p>Square des Martyrs, 6000 Charleroi</p></div>
+            <img src="assets/img/maps.svg" alt="round_place">
+            <p>Square des Martyrs, 6000 Charleroi</p>
+        </div>
         <div class="img_phone">
-        <img src="../../public/assets/img/phone.svg" alt="phone"><p>(123) 456-7890</p></div>
+            <img src="assets/img/phone.svg" alt="phone">
+            <p>(123) 456-7890</p>
+        </div>
         <div class="img_print">
-        <img src="../../public/assets/img/print.svg" alt="print"> <p>(123) 456-7890</p></div>
-    
+            <img src="assets/img/print.svg" alt="print">
+            <p>(123) 456-7890</p>
+        </div>
+
         <div class="logo_social_media">
-        <p>Social Media</p>
-        <img src="../../public/assets/img/Facebook.svg" alt="logo_facebook">
-        <img src="../../public/assets/img/Twitter.svg" alt="logo_twitter">
-        <img src="../../public/assets/img/LinkedIn.svg" alt="logo_linkedin">
-        <img src="../../public/assets/img/Youtube.svg" alt="logo_youtube">
-        <img src="../../public/assets/img/Instagram.svg" alt="logo_instagram">
-        <img src="../../public/assets/img/Pinterest.svg" alt="logo_pinterest">
-        <img src="../../public/assets/img/GooglePlus.svg" alt="logo_google">
-        <img src="../../public/assets/img/wifi.svg" alt="logo_rss">
-</div>
+            <p>Social Media</p>
+            <img src="assets/img/Facebook.svg" alt="logo_facebook">
+            <img src="assets/img/Twitter.svg" alt="logo_twitter">
+            <img src="assets/img/LinkedIn.svg" alt="logo_linkedin">
+            <img src="assets/img/Youtube.svg" alt="logo_youtube">
+            <img src="assets/img/Instagram.svg" alt="logo_instagram">
+            <img src="assets/img/Pinterest.svg" alt="logo_pinterest">
+            <img src="assets/img/GooglePlus.svg" alt="logo_google">
+            <img src="assets/img/wifi.svg" alt="logo_rss">
+        </div>
     </div>
     <div class="footer__contact">
         <ul>
