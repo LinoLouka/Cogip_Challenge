e5d918d49b16d5faa2450449c067f0ce57d95182
DelphineLecorney <lecorney.delphine@gmail.com>
2023-07-10 14:38:52 +0200

    Aff fucntion set404

--- Routes/Routes.php
+++ Routes/Routes.php
@@ -8,37 +8,38 @@ use App\Controllers\ControllersInvoices;
 use App\Controllers\ControllersContacts;
 use App\Controllers\ControllersCompanies;
 
-
 $router = new Router();
 
-$router->get('/', function() {
-    (new HomeController)->index();
-});
 $router->get('/home', function() {
     (new HomeController)->index();
 });
+
 $router->get('/invoices', function() {
     (new ControllersInvoices)->show();
 });
+
 $router->get('/contacts', function() {
     (new ControllersContacts)->show();
 });
+
 $router->get('/companies', function() {
     (new ControllersCompanies)->show();
 });
-$router->get('/companies/{$id}', function($id) {
+
+$router->get('/companies/{id}', function($id) {
     (new ControllersCompanies)->showId($id);
 });
-$router->get('/contacts/{id}', function($id){
+
+$router->get('/contacts/{id}', function($id) {
     (new ControllersContacts)->showId($id);
 });
-$router->get('/invoices/{$id}', function($id) {
+
+$router->get('/invoices/{id}', function($id) {
     (new ControllersInvoices)->showId($id);
 });
-if (file_exists(__FILE__.'/Controllers')){
-    require_once($router);
-} else {
+
+$router->set404(function() {
     require_once(__ROOT__.'/Resources/views/404.php');
-}
+});
 
 $router->run();
