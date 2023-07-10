fa75bfa4866bc76249c2da9227c258e836e130b0
L0uka <loukacadaulino@gmail.com>
2023-07-10 15:22:57 +0200

    bem class

--- Resources/views/footer.php
+++ Resources/views/footer.php
@@ -4,29 +4,29 @@
         <h2>COGIP</h2>
     </div>
     <div class="footer__infos">
-        <div class="img_svg">
+        <div class="img-svg">
             <img src="assets/img/maps.svg" alt="round_place">
             <p>Square des Martyrs, 6000 Charleroi</p>
         </div>
-        <div class="img_phone">
+        <div class="img-phone">
             <img src="assets/img/phone.svg" alt="phone">
             <p>(123) 456-7890</p>
         </div>
-        <div class="img_print">
+        <div class="img-print">
             <img src="assets/img/print.svg" alt="print">
             <p>(123) 456-7890</p>
         </div>
 
-        <div class="logo_social_media">
+        <div class="logo-social-media">
             <p>Social Media</p>
             <a href="https://www.facebook.com"><img src="assets/img/Facebook.svg" alt="logo_facebook">
-            <a href="https://www.twitter.com"><img src="assets/img/Twitter.svg" alt="logo_twitter">
-            <a href="https://www.linkedin.com"><img src="assets/img/LinkedIn.svg" alt="logo_linkedin">
-            <a href="https://www.youtube.com"><img src="assets/img/Youtube.svg" alt="logo_youtube">
-            <a href="https://www.instagram.com"><img src="assets/img/Instagram.svg" alt="logo_instagram">
-            <a href="https://www.pinterest.com"><img src="assets/img/Pinterest.svg" alt="logo_pinterest">
-            <a href="https://www.google.com"><img src="assets/img/GooglePlus.svg" alt="logo_google">
-            <img src="assets/img/wifi.svg" alt="logo_rss">
+                <a href="https://www.twitter.com"><img src="assets/img/Twitter.svg" alt="logo_twitter">
+                    <a href="https://www.linkedin.com"><img src="assets/img/LinkedIn.svg" alt="logo_linkedin">
+                        <a href="https://www.youtube.com"><img src="assets/img/Youtube.svg" alt="logo_youtube">
+                            <a href="https://www.instagram.com"><img src="assets/img/Instagram.svg" alt="logo_instagram">
+                                <a href="https://www.pinterest.com"><img src="assets/img/Pinterest.svg" alt="logo_pinterest">
+                                    <a href="https://www.google.com"><img src="assets/img/GooglePlus.svg" alt="logo_google">
+                                        <img src="assets/img/wifi.svg" alt="logo_rss">
         </div>
     </div>
     <div class="footer__contact">
