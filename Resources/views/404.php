c85abbef5a24c23a6c56dcfb0890feab3727f94e
Ismaël Mesmoudi Atou <ismaelmesmoudiatou@gmail.com>
2023-07-12 14:45:13 +0200

    page 404

--- Resources/views/404.php
+++ Resources/views/404.php
@@ -1,4 +1,25 @@
-<?php
-echo 'PAGE NOT FOUND';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/404.css">
+</head>
+<body>
+<div id='oopss'>
+    <div id='error-text'>
+        <img src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg" alt="404">
+        <span>404 error</span>
+        <p class="p-a">Page not found</p>
+        <p class="p-b">Sorry, the page you are looking for doesn't exist</p>
+        <a href='#' class="back"></a>
+    </div>
+</div> 
+</body>
+</html>
+
+
+
 
