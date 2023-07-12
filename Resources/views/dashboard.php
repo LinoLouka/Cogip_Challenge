58092cb06fd02b096910ac365abbee045e55f83e
DelphineLecorney <lecorney.delphine@gmail.com>
2023-07-12 11:32:05 +0200

    Add links for edit

--- Resources/views/dashboard.php
+++ Resources/views/dashboard.php
@@ -9,25 +9,31 @@
     <!-- rajouter icone poubelle pour les boutons delete -->
     <form method="POST">
 <table>
-                    <tr>
-                        <th>Id</th>
-                        <th>Invoice number</th>
-                        <th>Dates due</th>
-                        <th>Created at</th>
-                        <th>Name</th>
+    <tr>
+        <th>Id</th>
+        <th>Invoice number</th>
+        <th>Dates due</th>
+        <th>Created at</th>
+        <th>Name</th>
 
-                    </tr>
-                    <?php foreach ($invoices as $invoice):  ?>
+    </tr>
+    <?php foreach ($invoices as $invoice):  ?>
 
-                    <tr>
-                        <td><?php echo $invoice['id']; ?></td>
-                        <td value=<?php $invoice['id_company']?> name="invoiceIdCompany"><?php echo $invoice['id_company']; ?></td>
-                        <td><?php echo $invoice['created_at']; ?></td>
-                        <td><?php echo $invoice['updated_at']; ?></td>
-                        <td value=<?php $invoice['name']?> name="invoiceName"><?php echo $invoice['name']; ?></td>
-                        <td><button type="submit" value="">Edit</button> <button type="submit" value="delete">delete</button></td>       
-                        
-                    </tr>
+    <tr>
+        <td><?php echo $invoice['id']; ?></td>
+        <td value="<?php echo $invoice['id_company']; ?>" name="invoiceIdCompany">
+            <a href="edit.php?invoice=<?php echo $invoice['id']; ?>"><?php echo $invoice['id_company']; ?></a>
+        </td>
+        <td><?php echo $invoice['created_at']; ?></td>
+        <td><?php echo $invoice['updated_at']; ?></td>
+        <td value="<?php echo $invoice['name']; ?>" name="invoiceName">
+            <a href="edit.php?invoice=<?php echo $invoice['id']; ?>"><?php echo $invoice['name']; ?></a>
+        </td>
+    <td>
+        <button type="submit" value="">Edit</button>
+        <button type="submit" value="delete">Delete</button>
+    </td>
+    </tr>
                     <?php endforeach; ?>
                     </table>    
     </form>
@@ -45,10 +51,14 @@
                     <?php foreach ($contacts as $contact):  ?>
 
                     <tr>
-                        <td><?php echo $contact['name']; ?></td>
+                        <td value="<?php echo $contact['name'] ?>" name="contactName">
+                            <a href="edit.php?contact=<?php echo $contact['id'] ?>"><?php $contact['name']; ?></a>
+                        </td>
                         <td><?php echo $contact['phone']; ?></td>
                         <td><?php echo $contact['email']; ?></td>
-                        <td><?php echo $contact['company_id']; ?></td> 
+                        <td value="<?php echo $contact['company_id']; ?>" name="contactCompanyId">
+                            <a href="edit.php?contact=<?php echo $contact['id']; ?>"><?php echo $contact['company_id']; ?></a>
+                        </td>
                         <td><?php echo $contact['created_at']; ?></td>
                         <td><button type="submit" value="">Edit</button> <button type="submit" value="delete">delete</button></td>
                     </tr>
@@ -67,15 +77,20 @@
 
                     </tr>
                     <?php foreach ($companies as $company):  ?>
-                <tr>
-                    <td><?php echo $company['id']; ?></td>
-                    <td><?php echo $company['tva']; ?></td>
-                    <td><?php echo $company['name']; ?></td>
-                    <td><?php echo $company['country']; ?></td>
-                    <td><?php echo $company['type_id']; ?></td>
-                    <td><?php echo $company['created_at']; ?></td>
-                    <td><button type="submit" value="">Edit</button> <button type="submit" value="delete">delete</button></td>                
-                </tr>
+                        <tr>
+    <td><?php echo $company['id']; ?></td>
+    <td><?php echo $company['tva']; ?></td>
+    <td>
+        <a href="edit.php?company=<?php echo $company['id']; ?>"><?php echo $company['name']; ?></a>
+    </td>
+    <td><?php echo $company['country']; ?></td>
+    <td><?php echo $company['type_id']; ?></td>
+    <td><?php echo $company['created_at']; ?></td>
+    <td>
+        <button type="submit" value="">Edit</button>
+        <button type="submit" value="delete">Delete</button>
+    </td>
+</tr>
 
                 <?php endforeach; ?>
                 </table>
