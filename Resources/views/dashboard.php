89a6ae6f2279ad5e0f84b93ef46fdbb647a6d893
ethanosaure <ethandiasmarques.pro@gmail.com>
2023-07-12 15:33:54 +0200

    adding save btn when row clicked

--- Resources/views/dashboard.php
+++ Resources/views/dashboard.php
@@ -8,157 +8,172 @@
 <body>
     <!-- rajouter icone poubelle pour les boutons delete -->
 
-                    <table>
-                    <tr>
-                        <th>Name</th>
-                        <th>Phone</th>
-                        <th>Mail</th>
-                        <th>Company</th>
-                        <th>Created at</th>
-                    </tr>
-                    <?php foreach ($contacts as $contact):  ?>
+<table>
+<tr>
+    <th>Name</th>
+    <th>Phone</th>
+    <th>Mail</th>
+    <th>Company</th>
+    <th>Created at</th>
+</tr>
+<?php foreach ($contacts as $contact):  ?>
 
-                    <tr>
-                        <td><?php echo $contact['name']; ?></td>
-                        <td><?php echo $contact['phone']; ?></td>
-                        <td><?php echo $contact['email']; ?></td>
-                        <td><?php echo $contact['company_id']; ?></td> 
-                        <td><?php echo $contact['created_at']; ?></td>
-                        <td><button type="submit" value="">Edit</button> <button type="submit" value="delete">delete</button></td>
-                    </tr>
-                
-                    <?php endforeach; ?>
-                    </table>
-                    <table>
-                    <tr>
-                        <th>Id</th>
-                        <th>TVA</th>
-                        <th>Name</th>
-                        <th>Country</th>
-                        <th>Type</th>
-                        <th>Created at</th>
-                    </tr>
-                    <?php foreach ($companies as $company):  ?>
-                <tr>
-                    <td><?php echo $company['id']; ?></td>
-                    <td><?php echo $company['tva']; ?></td>
-                    <td><?php echo $company['name']; ?></td>
-                    <td><?php echo $company['country']; ?></td>
-                    <td><?php echo $company['type_id']; ?></td>
-                    <td><?php echo $company['created_at']; ?></td>
-                    <td><button type="submit" value="">Edit</button> <button type="submit" value="delete">delete</button></td>                
-                </tr>
+<tr>
+    <td><?php echo $contact['name']; ?></td>
+    <td><?php echo $contact['phone']; ?></td>
+    <td><?php echo $contact['email']; ?></td>
+    <td><?php echo $contact['company_id']; ?></td> 
+    <td><?php echo $contact['created_at']; ?></td>
+    <td><button type="submit" value="">Edit</button> <button type="submit" value="delete">delete</button></td>
+</tr>
 
-                <?php endforeach; ?>
-                </table>
+<?php endforeach; ?>
+</table>
+<table>
+<tr>
+    <th>Id</th>
+    <th>TVA</th>
+    <th>Name</th>
+    <th>Country</th>
+    <th>Type</th>
+    <th>Created at</th>
+</tr>
+<?php foreach ($companies as $company):  ?>
+<tr>
+<td><?php echo $company['id']; ?></td>
+<td><?php echo $company['tva']; ?></td>
+<td><?php echo $company['name']; ?></td>
+<td><?php echo $company['country']; ?></td>
+<td><?php echo $company['type_id']; ?></td>
+<td><?php echo $company['created_at']; ?></td>
+<td><button type="submit" value="">Edit</button> <button type="submit" value="delete">delete</button></td>                
+</tr>
+
+<?php endforeach; ?>
+</table>
                     
                    
-    <form method="POST" action="dashboard">
-    <table>
-        <tr>
-            <th>Id</th>
-            <th>Invoice number</th>
-            <th>Dates due</th>
-            <th>Created at</th>
-            <th>Name</th>
+<form method="POST" action="dashboard">
+<table>
+    <tr>
+        <th>Id</th>
+        <th>Invoice number</th>
+        <th>Dates due</th>
+        <th>Created at</th>
+        <th>Name</th>
+    </tr>
+    <?php foreach ($invoices as $invoice): ?>
+        <tr onclick='createSaveBtn(this)'>
+            <td><?php echo $invoice['id']; ?></td>
+            <td onclick='createInputCell(this)' ><?php echo $invoice['id_company']; ?></td>
+            <td><?php echo $invoice['created_at']; ?></td>
+            <td><?php echo $invoice['updated_at']; ?></td>
+            <td onclick='createInputCell(this)'><?php echo $invoice['name']; ?></td>
+            <td>
+            <input type="text" name="invoiceName[<?php echo $invoice['id']; ?>]" value="<?php echo $invoice['name']; ?>">
+            <input type="text" name="id_company[<?php echo $invoice['id']; ?>]" value="<?php echo $invoice['id_company']; ?>">
+            </td>
+            <td>
+                <button type="submit" name="editInvoice" value="<?php echo $invoice['id']; ?>">Edit</button>
+                <button type="submit" name="deleteInvoice" value="<?php echo $invoice['id']; ?>">Delete</button>
+            </td>
         </tr>
-        <?php foreach ($invoices as $invoice): ?>
-            <tr>
-                <td><?php echo $invoice['id']; ?></td>
-                <td onclick='createInputCell(this)' ><?php echo $invoice['id_company']; ?></td>
-                <td><?php echo $invoice['created_at']; ?></td>
-                <td><?php echo $invoice['updated_at']; ?></td>
-                <td onclick='createInputCell(this)'><?php echo $invoice['name']; ?></td>
-                <td>
-                <input type="text" name="invoiceName[<?php echo $invoice['id']; ?>]" value="<?php echo $invoice['name']; ?>">
-                <input type="text" name="id_company[<?php echo $invoice['id']; ?>]" value="<?php echo $invoice['id_company']; ?>">
-                </td>
-                <td>
-                    <button type="submit" name="editInvoice" value="<?php echo $invoice['id']; ?>">Edit</button>
-                    <button type="submit" name="deleteInvoice" value="<?php echo $invoice['id']; ?>">Delete</button>
-                </td>
-            </tr>
-        <?php endforeach; ?>
-    </table>    
-      <script>
-                            function createInputCell(cell){
-                                let content = cell.innerText;
+    <?php endforeach; ?>
+</table>    
+    <script>
+    function createSaveBtn(line){
+        let table = line.parentNode;
+        let existingBtn = table.querySelector('.saveBtn');
+        if(!existingBtn){
+            let SaveBtn = document.createElement('button');
+        SaveBtn.innerText = 'Save';
+        SaveBtn.className = 'saveBtn';
+        table.appendChild(SaveBtn);
 
-                                let input = document.createElement('input');
-                                input.type='text';
-                                input.value=content;
-
-                                cell.innerText = '';
-                                cell.appendChild(input);
-
-                                input.focus();
-                            input.addEventListener('keydown', (event) =>{
-                                if(event.code == 13){
-                                    console.log('press Enter');
-                                }
-                            });
-                            }
-                        </script>
-                </form>
-  
-  
-  
-
-                   <?php 
-                    echo "<br>";
-                    echo "<br>";
-                    echo "<br>";
-                    ?>
-                    <!-- mettre dans une autre section -->
-                         <!-- fonction add Contacts -->
-                    <!-- <form method="POST" action="."> -->
-                        <form>
-                        <label for="contactName">Contact name:</input>
-                            <input name="contactName" type="text" value="">
-                        <label for="contactPhone">Contact phone:</input>
-                            <input name="contactPhone" type="number" value="">
-                        <label for="contactMail">Contact mail:</label>
-                            <input name="contactMail" value="" type="email">
-                        <label for="contactCompanyId">Contact company:</label>
-                            <input name="contactCompanyId" value="" type="number">
-
-                        <button type="submit" name="validationContact">Validation contact</button>
-                    </form>
-                       <?php 
-                    echo "<br>";
-                    echo "<br>";
-                    echo "<br>";
-                    ?>
-
-
-                    <!-- mettre dans une autre section -->
-                        <!-- fonction add Companies -->
-                    <form method="POST" action="dashboard">
-                        <label for="companyName">Company name:</label>
-                            <input name="companyName" type="text" value="">
-                        <label for="companyType">Company type:</label>
-                            <input name="companyType" type="number" value="">
-                        <label for="companyCountry">Country of the company:</label>
-                            <input name="companyCountry" type="text" value="">
-                        <label for="companyTVA">TVA:</label>
-                            <input name="companyTVA" type="text" value="">
-                        
-                            <button type="submit" name="validationCompany">Validation Company</button>
-                    </form>
-              <form>
-                    <!-- mettre dans une section -->
-                    <!-- fonction add invoices -->
-                    <!-- <form method="POST" action="."> -->
-                        <label for="invoicesNumber">Invoice number:</input>
-                            <input name="invoicesNumber" type="text" value="">
-                        <label for="invoicesCompany">Invoice Company name:</input>
-                            <input name="invoicesCompany" type="text" value="">
-
-                            <button type="submit" name="validationInvoice">Validation Invoice</button>
-                    </form>
-                   <?php
+        }
+        
+    }
                     
-                    ?>
+        function createInputCell(cell){
+    let content = cell.innerText;
+    let input = document.createElement('input');
+    input.type='text';
+    input.value=content;
+    cell.innerText = '';
+
+    cell.appendChild(input);
+
+
+    input.focus();
+    input.addEventListener('keydown', (event) =>{
+    if(event.keyCode == 13){
+    cell.innerText = input.value;
+    input.remove();
+    }
+});
+
+}
+</script>
+</form>
+
+
+
+
+    <?php 
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    ?>
+    <!-- mettre dans une autre section -->
+            <!-- fonction add Contacts -->
+    <!-- <form method="POST" action="."> -->
+        <form>
+        <label for="contactName">Contact name:</input>
+            <input name="contactName" type="text" value="">
+        <label for="contactPhone">Contact phone:</input>
+            <input name="contactPhone" type="number" value="">
+        <label for="contactMail">Contact mail:</label>
+            <input name="contactMail" value="" type="email">
+        <label for="contactCompanyId">Contact company:</label>
+            <input name="contactCompanyId" value="" type="number">
+
+        <button type="submit" name="validationContact">Validation contact</button>
+    </form>
+        <?php 
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    ?>
+
+
+    <!-- mettre dans une autre section -->
+        <!-- fonction add Companies -->
+    <form method="POST" action="dashboard">
+        <label for="companyName">Company name:</label>
+            <input name="companyName" type="text" value="">
+        <label for="companyType">Company type:</label>
+            <input name="companyType" type="number" value="">
+        <label for="companyCountry">Country of the company:</label>
+            <input name="companyCountry" type="text" value="">
+        <label for="companyTVA">TVA:</label>
+            <input name="companyTVA" type="text" value="">
+        
+            <button type="submit" name="validationCompany">Validation Company</button>
+    </form>
+<form>
+    <!-- mettre dans une section -->
+    <!-- fonction add invoices -->
+    <!-- <form method="POST" action="."> -->
+        <label for="invoicesNumber">Invoice number:</input>
+            <input name="invoicesNumber" type="text" value="">
+        <label for="invoicesCompany">Invoice Company name:</input>
+            <input name="invoicesCompany" type="text" value="">
+
+            <button type="submit" name="validationInvoice">Validation Invoice</button>
+    </form>
+    <?php
+    
+    ?>
 
 
 </table>
