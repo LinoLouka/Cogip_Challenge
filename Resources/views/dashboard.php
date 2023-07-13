11461e53df00b52d31dda05ae02058948aa6aed5
DelphineLecorney <lecorney.delphine@gmail.com>
2023-07-13 11:52:23 +0200

    Delete a few columns dashboard

--- Resources/views/dashboard.php
+++ Resources/views/dashboard.php
@@ -15,17 +15,12 @@
         <th>Name</th>
         <th>Phone</th>
         <th>Mail</th>
-        <th>Company</th>
-        <th>Created at</th>
     </tr>
     <?php foreach ($contacts as $contact):  ?>
     <tr>      
         <td onclick='createInputCell(this)'><?php echo $contact['name']; ?></td>
         <td><?php echo $contact['phone']; ?></td>
         <td><?php echo $contact['email']; ?></td>
-        <td onclick='createInputCell(this)' ><?php echo $contact['company_id']; ?></td>
-        <td><?php echo $contact['created_at']; ?></td>
-        <td><?php echo $contact['updated_at']; ?></td>
         <td>
         <input type="text" name="contactName[<?php echo $contact['id']; ?>]" value="<?php echo $contact['name']; ?>">
         <input type="text" name="contactCompanyId[<?php echo $contact['id']; ?>]" value="<?php echo $contact['company_id']; ?>">
@@ -42,22 +37,17 @@
 <form method="POST" action="dashboard">
 <table>
     <tr>
-        <th>Id</th>
-        <th>TVA</th>
         <th>Name</th>
+        <th>TVA</th>   
         <th>Country</th>
-        <th>Type</th>
-        <th>Created at</th>
     </tr>
     <?php foreach ($companies as $company):  ?>
 
-    <tr>   
-        <td><?php echo $company['id']; ?></td>   
-        <td><?php echo $company['tva']; ?></td>
+    <tr>    
+        
         <td onclick='createInputCell(this)'><?php echo $company['name']; ?></td>
+        <td><?php echo $company['tva']; ?></td>
         <td><?php echo $company['country']; ?></td>
-        <td><?php echo $company['type_id']; ?></td>
-        <td><?php echo $company['created_at']; ?></td>
         <td><input type="text" name="companyName[<?php echo $company['id']; ?>]" value="<?php echo $company['name']; ?>"></td>
         <td>
             <button type="submit" name="editCompany" value="<?php echo $company['id']; ?>">Edit</button>
@@ -71,18 +61,15 @@
 <form method="POST" action="dashboard">
 <table>
     <tr>
-        <th>Id</th>
         <th>Invoice number</th>
         <th>Dates due</th>
-        <th>Created at</th>
         <th>Name</th>
     </tr>
     <?php foreach ($invoices as $invoice): ?>
         <tr>
-            <td><?php echo $invoice['id']; ?></td>
+
             <td onclick='createInputCell(this)' ><?php echo $invoice['id_company']; ?></td>
             <td><?php echo $invoice['created_at']; ?></td>
-            <td><?php echo $invoice['updated_at']; ?></td>
             <td onclick='createInputCell(this)'><?php echo $invoice['name']; ?></td>
             <td>
             <input type="text" name="invoiceName[<?php echo $invoice['id']; ?>]" value="<?php echo $invoice['name']; ?>">
