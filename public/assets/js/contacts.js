984d67bd74b7c245c55300ec036c9a9794fdec36
Ismaël Mesmoudi Atou <ismaelmesmoudiatou@gmail.com>
2023-07-14 11:11:53 +0200

    tri fonctionnel

--- public/assets/js/contacts.js
+++ public/assets/js/contacts.js
@@ -1,20 +1,97 @@
-function searchContacts() {
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("search_contacts");
-    filter = input.value.toUpperCase();
-    table = document.querySelector(".section-contacts__table table");
-    tr = table.getElementsByTagName("tr");
-  
-   
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[1];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
+function sortContacts() {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.querySelector(".section-contacts__table table");
+  switching = true;
+
+  var sortBy = document.getElementById("sort_contacts").value; // Récupérer la valeur de tri sélectionnée
+
+  while (switching) {
+    switching = false;
+    rows = table.rows;
+
+    for (i = 1; i < (rows.length - 1); i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("td")[1];
+      y = rows[i + 1].getElementsByTagName("td")[1];
+
+      // Effectuer le tri en fonction du critère sélectionné
+      switch (sortBy) {
+        case "id":
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            shouldSwitch = true;
+          }
+          break;
+        case "name":
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            shouldSwitch = true;
+          }
+          break;
+        case "phone":
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            shouldSwitch = true;
+          }
+          break;
+        case "email":
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            shouldSwitch = true;
+          }
+          break;
+        case "company":
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            shouldSwitch = true;
+          }
+          break;
+        case "created_at":
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            shouldSwitch = true;
+          }
+          break;
+      }
+
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
       }
     }
   }
+
+  // Afficher uniquement la colonne sélectionnée
+  var columnIndex = getSelectedColumnIndex(sortBy);
+  showSelectedColumn(columnIndex);
+}
+
+function getSelectedColumnIndex(sortBy) {
+  switch (sortBy) {
+    case "id":
+      return 0;
+    case "name":
+      return 1;
+    case "phone":
+      return 2;
+    case "email":
+      return 3;
+    case "company":
+      return 4;
+    case "created_at":
+      return 5;
+    default:
+      return -1;
+  }
+}
+
+function showSelectedColumn(columnIndex) {
+  var table = document.querySelector(".section-contacts__table table");
+  var rows = table.rows;
+
+  for (var i = 0; i < rows.length; i++) {
+    var cells = rows[i].getElementsByTagName("td");
+
+    for (var j = 0; j < cells.length; j++) {
+      if (j === columnIndex) {
+        cells[j].style.display = "";
+      } else {
+        cells[j].style.display = "none";
+      }
+    }
+  }
+}
--- public/assets/js/invoices.js
+++ public/assets/js/invoices.js
@@ -1,23 +1,49 @@
-function searchInvoices() {
-  var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("search_invoices");
-  filter = input.value.toUpperCase();
-  table = document.querySelector(".section-invoices__table table");
-  tr = table.getElementsByTagName("tr");
-
-  for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[3]; 
-      if (td) {
-          txtValue = td.textContent || td.innerText;
-          if (txtValue.toUpperCase().indexOf(filter) > -1) {
-              tr[i].style.display = "";
-          } else {
-              tr[i].style.display = "none";
-          }
+function sortInvoices() {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.querySelector(".section-invoices__table table");
+    switching = true;
+  
+    while (switching) {
+      switching = false;
+      rows = table.rows;
+  
+      for (i = 1; i < (rows.length - 1); i++) {
+        shouldSwitch = false;
+        x = rows[i].getElementsByTagName("td")[3]; 
+        y = rows[i + 1].getElementsByTagName("td")[3]; 
+  
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
       }
+  
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+      }
+    }
   }
-}
-
-
-
+  
+  function searchInvoices() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("search_invoices");
+    filter = input.value.toUpperCase();
+    table = document.querySelector(".section-invoices__table table");
+    tr = table.getElementsByTagName("tr");
+  
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1]; 
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  
+    sortInvoices(); // Appeler la fonction pour trier le tableau après la recherche
+  }
   
