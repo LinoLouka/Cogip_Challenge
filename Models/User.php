236ad4837635c73e62c133d8f82c9348e35cbef2
DelphineLecorney <lecorney.delphine@gmail.com>
2023-07-15 17:27:27 +0200

    Fix SANITIZE

--- Models/User.php
+++ Models/User.php
@@ -15,18 +15,18 @@ class User
 
     public function saveUser($role_id, $firstname, $lastname, $email, $password)
     {
-            // if($this->isEmailExists($email)) {
-            //     return false;
-            // }
+            if($this->isEmailExists($email)) {
+                return false;
+            }
 
             $request = 'INSERT INTO users(role_id, first_name, last_name, email, password, created_at, updated_at)
                      VALUES (:role_id, :firstname, :lastname, :email, :password, NOW(), NOW())';
             $statement = $this->bdd->prepare($request);
 
-            // $firstname = filter_var($firstname, FILTER_SANITIZE_STRING);
-            // $lastname = filter_var($lastname, FILTER_SANITIZE_STRING);
-            // $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-            // $password = password_hash($password, PASSWORD_DEFAULT);
+            $firstname = filter_var($firstname, FILTER_SANITIZE_STRING);
+            $lastname = filter_var($lastname, FILTER_SANITIZE_STRING);
+            $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+            $password = password_hash($password, PASSWORD_DEFAULT);
 
             $statement->bindValue(':role_id', $role_id, \PDO::PARAM_INT);
             $statement->bindValue(':firstname', $firstname, \PDO::PARAM_STR);
@@ -34,6 +34,7 @@ class User
             $statement->bindValue(':email', $email, \PDO::PARAM_STR);
             $statement->bindValue(':password', $password, \PDO::PARAM_STR);
             $statement->execute();
+
     }
 
     public function getUsersRoles()
@@ -48,17 +49,14 @@ class User
             return $statement->fetchAll(\PDO::FETCH_ASSOC);
     }
 
+    public function isEmailExists($email)
+    {
+        $request = 'SELECT COUNT(*) FROM users WHERE email = :email';
+        $statement =$this->bdd->prepare($request);
+        $statement->bindValue(':email', $email, \PDO::PARAM_STR);
+        $statement->execute();
+        $count = $statement->fetch(\PDO::FETCH_COLUMN);
 
-
-
-    // public function isEmailExists($email)
-    // {
-    //     $request = 'SELECT COUNT(*) FROM users WHERE email = :email';
-    //     $statement =$this->bdd->prepare($request);
-    //     $statement->bindValue(':email', $email, \PDO::PARAM_STR);
-    //     $statement->execute();
-    //     $count = $statement->fetch(PDO::FETCH_COLUMN);
-
-    //     return ($count > 0);
-    // }
+        return ($count > 0);
+    }
 }
