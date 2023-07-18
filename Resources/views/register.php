eb939525699c8ee5d06cd3d5f4f18e53567cf650
Ismaël Mesmoudi Atou <ismaelmesmoudiatou@gmail.com>
2023-07-18 15:04:15 +0200

    register

--- Resources/views/register.php
+++ Resources/views/register.php
@@ -11,6 +11,8 @@ $formSubmitted = ($_SERVER['REQUEST_METHOD'] === 'POST') && empty($lastnameError
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/register.css">
     <title>Register</title>
     <style>
         .error {
